03f1a3dba48a093d9029ec95c7fd49b33d54d792
Noel Keener <noel@nakeener.com>
2019-11-15 13:05:07 -0600

    Completed build
    Built application capture and save functions. Styled it in a god-awful pink, for some reason.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,55 @@
 const videoElement = document.querySelector('#viewWindow');
+const downloadElement = document.querySelector('#clip-files');
 const consoleElement = document.querySelector('#console');
+const controlButtonElement = document.getElementsByName('control__button');
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button =>
   button.addEventListener('click', e => handleClick(e), false)
 );
+let videoTracks = [];
+let mediaRecorder;
 
 const handleClick = e => {
   const button = e.target;
   const buttonId = e.target.id;
-  if (buttonId === 'record') {
-    button.id = 'stop';
-    document.querySelector('#save').disabled = true;
-    util.addClass(button, 'pressed');
-    startCapture();
-  } else if (buttonId === 'stop') {
-    button.id = 'record';
-    document.querySelector('#save').disabled = false;
-    util.removeClass(button, 'pressed');
-    stopCapture();
-  } else {
-    util.addClass(button, 'pressed');
-    setTimeout(() => util.removeClass(button, 'pressed'), 200);
-    util.console.log('Saving...');
-    // do saving
+  switch (buttonId) {
+    case 'record':
+    case 'record__button--indicator':
+      if (buttonId == 'record') {
+        button.id = 'stop';
+        button.firstElementChild.id = 'stop__button--indicator';
+        util.addClass(button, 'pressed');
+      } else {
+        const parent = button.parentNode;
+        button.id = 'stop__button--indicator';
+        parent.id = 'stop';
+        util.addClass(parent, 'pressed');
+      }
+      startCapture();
+      break;
+    case 'stop':
+    case 'stop__button--indicator':
+      if (buttonId == 'stop') {
+        button.id = 'record';
+        button.firstElementChild.id = 'record__button--indicator';
+        util.removeClass(button, 'pressed');
+      } else {
+        const parent = button.parentNode;
+        button.id = 'record__button--indicator';
+        button.parentNode.id = 'record';
+        util.removeClass(parent, 'pressed');
+      }
+      stopCapture();
+      break;
+    case 'console__toggle':
+      toggleConsole(e.target);
+      break;
+    case 'console__toggle--knob':
+      toggleConsole(e.target.parentNode);
+      break;
+    default:
+      util.console.warn(`Somehow, you pressed a button that doesn't exist.`);
+      break;
   }
 };
 
@@ -45,6 +72,18 @@ const util = {
   },
 };
 
+const toggleConsole = toggle => {
+  const isOn = toggle.classList.contains('on');
+  if (isOn) {
+    util.removeClass(toggle, 'on');
+    util.removeClass(consoleElement, 'show');
+  } else {
+    util.addClass(toggle, 'on');
+    util.addClass(consoleElement, 'show');
+  }
+  return true;
+};
+
 const displayMediaOptions = {
   video: {
     cursor: 'always',
@@ -53,33 +92,54 @@ const displayMediaOptions = {
 };
 
 async function startCapture() {
-  util.console.log('Recording...');
-  util.console.warn('Save button disabled.');
-
   try {
     videoElement.srcObject = await navigator.mediaDevices.getDisplayMedia(
       displayMediaOptions
     );
-    dumpOptionsInfo();
+    mediaRecorder = new MediaRecorder(videoElement.srcObject);
+    mediaRecorder.mimeType = 'image/gif';
+    mediaRecorder.ondataavailable = blob => {
+      videoTracks.push(URL.createObjectURL(blob.data));
+    };
+    mediaRecorder.start();
+    util.console.log('Recording...');
   } catch (err) {
     util.console.error(`Error: ${err}`);
+    util.console.info(
+      "You either denied the request for screen capture, or you're using garbage Internet Explorer."
+    );
+    util.console.warn(
+      "If you're using Internet Explorer, stop. It's made of garbage and it hates you. Stop using it."
+    );
   }
 }
 
 const stopCapture = e => {
-  let tracks = videoElement.srcObject.getTracks();
+  mediaRecorder.stop();
+  util.console.log('Stopped.');
+  util.console.log(videoTracks);
+  const clipName = prompt('Enter a name for your video.');
+  util.console.log(
+    `${clipName} successfully recorded. See My Clips to download.`
+  );
 
-  tracks.forEach(track => track.stop());
-  videoElement.srcObject = null;
-};
-
-const dumpOptionsInfo = () => {
-  const videoTrack = videoElement.srcObject.getVideoTracks()[0];
-  console.log(videoTrack);
-  util.console.log('Track settings:');
-  util.console.info(JSON.stringify(videoTrack.getSettings(), null, 2));
-  util.console.log('Track constraints:');
-  util.console.info(JSON.stringify(videoTrack.getConstraints(), null, 2));
+  try {
+    videoTracks.forEach(track => {
+      const videoUrl = track;
+      const videoUrlElement = document.createElement('a');
+      const videoTitleElement = document.createElement('li');
+      videoUrlElement.href = videoUrl;
+      videoUrlElement.setAttribute('download', clipName.split(' ').join(''));
+      videoUrlElement.id = clipName.split(' ').join('');
+      videoTitleElement.textContent = clipName;
+      videoUrlElement.appendChild(videoTitleElement);
+      downloadElement.appendChild(videoUrlElement);
+      videoElement.srcObject = null;
+    });
+    videoTracks = [];
+  } catch (err) {
+    util.console.error(err);
+  }
 };
 
 (() => {
